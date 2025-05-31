988ea467cde1899a180f266d2fda162f715207ac
Mohammed Maaz <171570509+Mohammedmaaz541@users.noreply.github.com.>
2025-05-31 21:47:28 +0530

    changing in flask

--- html_python.py
+++ html_python.py
@@ -4,12 +4,5 @@ app=Flask(__name__)
 def index():
     return render_template('index.html')
 
-@app.route('/add')
-def addition():
-    var_1=int(request.args.get('a'))
-    var_2=int(request.args.get('b'))
-    sum=var_1+var_2
-    return str(sum)
-
 if __name__=="__main__":
     app.run(debug=True)
